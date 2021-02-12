78921152d504725e74ebc71ae2f1a1366ca1e76c
nearnshaw <nearnshaw@decentraland.org>
2021-02-12 15:59:16 -0300

    new utils lib version

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,10 @@
-import { BulletMark } from "./bullet"
-import { Sound } from "./sound"
-import utils from "../node_modules/decentraland-ecs-utils/index"
+import { BulletMark } from './bullet'
+import { Sound } from './sound'
+import * as utils from '@dcl/ecs-scene-utils'
 
 // Base scene
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseLight.glb"))
+base.addComponent(new GLTFShape('models/baseLight.glb'))
 engine.addEntity(base)
 
 // Shooting area
@@ -23,31 +23,31 @@ shootingArea.addComponent(redMaterial)
 engine.addEntity(shootingArea)
 
 // Create trigger for shooting area
-let triggerBox = new utils.TriggerBoxShape(new Vector3(16, 16, 4), Vector3.Zero())
+let triggerBox = new utils.TriggerBoxShape(
+  new Vector3(16, 16, 4),
+  Vector3.Zero()
+)
 
 shootingArea.addComponent(
-  new utils.TriggerComponent(
-    triggerBox, 0, 0, null, null,
-    () => {
-      // Player enter
+  new utils.TriggerComponent(triggerBox, {
+    onCameraEnter: () => {
       isPlayerInShootingArea = true
       shootingArea.getComponent(Material).emissiveColor = Color3.Yellow()
     },
-    () => {
-      // Player exit
+    onCameraExit: () => {
       isPlayerInShootingArea = false
       shootingArea.getComponent(Material).emissiveColor = Color3.Black()
-    }
-  )
+    },
+  })
 )
 
 // Sounds
-const shotSound = new Sound(new AudioClip("sounds/shotSilencer.mp3"))
-const shotRicochetSound = new Sound(new AudioClip("sounds/shotRicochet.mp3"))
-const shotFailSound = new Sound(new AudioClip("sounds/shotFail.mp3"))
+const shotSound = new Sound(new AudioClip('sounds/shotSilencer.mp3'))
+const shotRicochetSound = new Sound(new AudioClip('sounds/shotRicochet.mp3'))
+const shotFailSound = new Sound(new AudioClip('sounds/shotFail.mp3'))
 
 // Cache bullet mark on load otherwise the first bullet mark won't appear instantly when fired
-const bulletMarkShape = new GLTFShape("models/bulletMark.glb")
+const bulletMarkShape = new GLTFShape('models/bulletMark.glb')
 const bulletMarkCache = new BulletMark(bulletMarkShape)
 bulletMarkCache.getComponent(Transform).scale.setAll(0)
 
@@ -56,14 +56,16 @@ const input = Input.instance
 const DELETE_TIME = 8 // In seconds
 let isPlayerInShootingArea = false
 
-input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, (event) => {
+input.subscribe('BUTTON_DOWN', ActionButton.POINTER, true, (event) => {
   if (isPlayerInShootingArea) {
     shotSound.getComponent(AudioSource).playOnce()
     if (engine.entities[event.hit.entityId] != undefined) {
       let entity = engine.entities[event.hit.entityId]
       shotRicochetSound.getComponent(AudioSource).playOnce()
       // Calculate the position of where the bullet hits relative to the target
-      entity.hasComponent(utils.KeepRotatingComponent) ? hitDynamicTarget(entity, event) : hitStaticTarget(event)
+      entity.hasComponent(utils.KeepRotatingComponent)
+        ? hitDynamicTarget(entity, event)
+        : hitStaticTarget(event)
     }
   } else {
     shotFailSound.getComponent(AudioSource).playOnce()
@@ -82,9 +84,11 @@ function hitDynamicTarget(entity: IEntity, event: LocalActionButtonEvent) {
   bulletMark.setParent(entity) // Make the bullet mark the child of the target so that it remains on the target
 
   // NOTE: If you have multiple parent entities then you'll need to subtract for each successive parent transform
-  let targetParentPosition = entity.getParent().getComponent(Transform).position 
+  let targetParentPosition = entity.getParent().getComponent(Transform).position
   let targetPosition = entity.getComponent(Transform).position
-  let relativePosition = event.hit.hitPoint.subtract(targetParentPosition).subtract(targetPosition)
+  let relativePosition = event.hit.hitPoint
+    .subtract(targetParentPosition)
+    .subtract(targetPosition)
   relativePosition.rotate(entity.getComponent(Transform).rotation.conjugate()) // Inversing the parent's rotation
   bulletMark.getComponent(Transform).position = relativePosition
 }
--- src/target.ts
+++ src/target.ts
@@ -1,13 +1,13 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
+import * as utils from '@dcl/ecs-scene-utils'
 
 export class Target extends Entity {
-  constructor(model: GLTFShape, transform: Transform, rotation?: Quaternion ) {
+  constructor(model: GLTFShape, transform: Transform, rotation?: Quaternion) {
     super()
     engine.addEntity(this)
     this.addComponent(model)
     this.addComponent(transform)
 
     // Move the targets back and forth between start and end positions
-    if(rotation) this.addComponent(new utils.KeepRotatingComponent(rotation))
+    if (rotation) this.addComponent(new utils.KeepRotatingComponent(rotation))
   }
 }
